841adb8e06c0ca8bda3ac7a631d88e62341154cd
Pierre Kurkdjian <pierrekurkdjian@hotmail.fr>
2019-06-04 14:14:16 -0700

    Update gmt_install_ext.sh

--- gmt_install_ext.sh
+++ gmt_install_ext.sh
@@ -7,9 +7,13 @@ else
 
     echo "Building and installing open62541 (OPCUA library) into $INSTALL_PREFIX"
 
-    #compilation
+    # compilation
     gcc -c -Wall -Werror -fpic src/open62541.c
-    gcc -shared -o INSTALL_PREFIX/ext/lib/libopen62541.so open62541.o
-    cp src/open62541.c INSTALL_PREFIX/ext/include/libopen62541.so
+    
+    # installation of the library (.so) in $GMT_LOCAL/ext/lib/
+    gcc -shared -o INSTALL_PREFIX/lib/libopen62541.so open62541.o
+    
+    # library header (.h) in $GMT_LOCAL/ext/include/
+    cp src/open62541.c INSTALL_PREFIX/include/libopen62541.so
 
 fi
